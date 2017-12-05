8ca890237a4b9f77c3c72a37d61586490cd93338
Gabriel <blackbird698@gmail.com>
2017-12-05 17:53:08 -0500

    initialize

---
+++ sorter.h
@@ -0,0 +1,72 @@
+#ifndef SORTER_H
+#define SORTER_H
+
+struct film
+{
+    int empty;
+
+    char * comparechar;
+    int compareint;
+    double comparedouble;
+
+    char * color;
+    char * director_name;
+    int num_critic_for_reviews;
+    int duration;
+    int director_facebook_likes;
+    int actor_3_facebook_likes;
+    char * actor_2_name;
+    int actor_1_facebook_likes;
+    int gross;
+    char * generes;
+    char * actor_1_name;
+    char * movie_title;
+    int num_voted_users;
+    int cast_total_facebook_likes;
+    char * actor_3_name;
+    int facenumber_in_poster;
+    char * plot_keywords;
+    char * movie_imdb_link;
+    int num_user_for_reviews;
+    char * language;
+    char * country;
+    char * content_rating;
+    int budget;
+    int title_year;
+    int actor_2_facebook_likes;
+    double imdb_score;
+    double aspect_ratio;
+    int movie_facebook_likes;
+    struct film *next;
+
+};
+
+struct list{
+    struct  film * start;
+    struct  film * end;
+};
+
+struct input{
+    void * p;
+    int com;
+};
+
+char* repath(const char *, const char*);
+char* cleanWord( char*);
+struct film * add(struct film*, int, char*);
+int comparestring(char*);
+void split(struct film*, struct film**, struct film**);
+struct film* sortedmerge(struct film*, struct film*);
+void mergeint(struct film**);
+struct film* sortedmergedouble(struct film*, struct film*);
+void mergedouble(struct film**);
+struct film* sortedmergestring(struct film*, struct film*);
+void mergestring(struct film**);
+void print2(struct film*);
+void print(struct film*, char*);
+void freelist(struct film*);
+void * addFile(void*);
+void * dirthread(void*);
+
+
+#endif
