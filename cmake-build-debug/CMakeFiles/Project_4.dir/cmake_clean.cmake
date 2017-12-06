6f6af454427dda07a5f650e5b6eaa9ab3230ba4b
Gabriel <blackbird698@gmail.com>
2017-12-06 15:53:21 -0500

    upload

---
+++ cmake-build-debug/CMakeFiles/Project_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_4.dir/sorter.c.o"
+  "Project_4.pdb"
+  "Project_4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
