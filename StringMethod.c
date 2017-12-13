dc2abd95986eb90b837b669d3b12ce8136d91324
GhostkidYao <32246410+GhostkidYao@users.noreply.github.com>
2017-12-12 18:03:19 -0800

    Update StringMethod.c

--- StringMethod.c
+++ StringMethod.c
@@ -9,14 +9,16 @@
 
 
 char* convertToString(char* col, FILE* fp, char* ip) {
-	char* ipLine = malloc(strlen(ip)+2);
+
+    printf("yes" );
+    char* ipLine = malloc(strlen(ip)+2);
     strcpy(ipLine, ip);
     strcat(ipLine, "<");
 
     char* colLine = malloc(strlen(ip)+2);
     strcpy(colLine, col);
     strcat(colLine, "~");
-
+    
     char* fullStr = (char*) malloc(5000);
     fullStr[0] = '\0';
 
@@ -24,8 +26,7 @@ char* convertToString(char* col, FILE* fp, char* ip) {
 
     char* newStr = (char*) malloc(500);
     newStr[0] = '\0';
-
-	while (fgets(newStr, 500, fp) != NULL) {
+    while (fgets(newStr, 500, fp) != NULL) {
         if (strlen(fullStr) + strlen(newStr) > allocationAmt - 10) {
             allocationAmt = allocationAmt * 2;
             char* tempStr = malloc(allocationAmt * 2);
@@ -36,17 +37,14 @@ char* convertToString(char* col, FILE* fp, char* ip) {
 
         strncat(fullStr, newStr, strlen(newStr)-1);
         strcat(fullStr, "^");
-	}
-
+    }
     strcat(fullStr, "@");
-
-
     char* returnStr = malloc(strlen(fullStr) + strlen(ipLine) + strlen(colLine)+100);
-    itoa(strlen(fullStr) + strlen(ipLine) + strlen(colLine) + 100, returnStr, 10);
+    sprintf(returnStr, "%d", (int)(strlen(fullStr) + strlen(ipLine) + strlen(colLine) + 100));
     strcat(returnStr, ">");
     strcat(returnStr, colLine);
     strcat(returnStr, fullStr);
     strcat(returnStr, ipLine);
 
     return returnStr;
-}
+} 
